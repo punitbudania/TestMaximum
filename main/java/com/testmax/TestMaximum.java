5afe9ccef62e72621d9d2e0f33f9d4c560d2f72a
punitbudania <punitbudania88@gmail.com>
2020-12-03 14:12:51 +0530

    [punit]Added: Find string max UC 3

--- main/java/com/testmax/TestMaximum.java
+++ main/java/com/testmax/TestMaximum.java
@@ -4,12 +4,13 @@ public class TestMaximum
 {
     static Integer[] numbers = {6, 7, 3};
     static Double[] floatnos = {4.6, 7.9, 9.5};
+    static String[] fruits = {"Mango", "Pineapple", "Banana"};
 
     public static void main(String[] args)
     {
         System.out.println(findMax(numbers));
         System.out.println(findMax(floatnos));
-
+        System.out.println(findMax(fruits));
     }
 
     public static Integer findMax(Integer[] array)
@@ -42,4 +43,18 @@ public class TestMaximum
         return max;
     }
 
+    public static String findMax(String[] array)
+    {
+        String max= array[0];
+
+        if ((max.compareTo(array[1])) < 0)
+        {
+            max = array[1];
+        }
+        if((max.compareTo(array[2])) < 0)
+        {
+            max = array[2];
+        }
+        return max;
+    }
 }
