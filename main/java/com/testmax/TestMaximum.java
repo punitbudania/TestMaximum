bab6fca4610ae361efc6b99979d0fda1152686ec
punitbudania <punitbudania88@gmail.com>
2020-12-03 13:51:50 +0530

    [punit]Added: Find float max UC 2

--- main/java/com/testmax/TestMaximum.java
+++ main/java/com/testmax/TestMaximum.java
@@ -3,15 +3,33 @@ package com.testmax;
 public class TestMaximum
 {
     static Integer[] numbers = {6, 7, 3};
+    static Double[] floatnos = {4.6, 7.9, 9.5};
 
     public static void main(String[] args)
     {
         System.out.println(findMax(numbers));
+        System.out.println(findMax(floatnos));
+
     }
 
     public static Integer findMax(Integer[] array)
     {
         Integer max= array[0];
+
+        if ((max.compareTo(array[1])) < 0)
+        {
+            max = array[1];
+        }
+        if((max.compareTo(array[2])) < 0)
+        {
+            max = array[2];
+        }
+        return max;
+    }
+
+    public static Double findMax(Double[] array)
+    {
+        Double max= array[0];
 
         if ((max.compareTo(array[1])) < 0)
         {
--- test/java/com/testcases/TestCases.java
+++ test/java/com/testcases/TestCases.java
@@ -32,4 +32,31 @@ public class TestCases
         Integer result = ThirdMax.findMax(ThirdTest);
         Assert.assertEquals(ThirdTest[2], result);
     }
+
+    Double[] DFirstTest = {8.1, 7.4, 3.4};
+    @Test
+    public void DMaxatFirstPosition()
+    {
+        TestMaximum DFirstMax = new TestMaximum();
+        Double result = DFirstMax.findMax(DFirstTest);
+        Assert.assertEquals(DFirstTest[0], result);
+    }
+
+    Double[] DSecondTest = {3.5, 6.7, 2.3};
+    @Test
+    public void DMaxatSecondPosition()
+    {
+        TestMaximum DSecondMax = new TestMaximum();
+        Double result = DSecondMax.findMax(DSecondTest);
+        Assert.assertEquals(DSecondTest[1], result);
+    }
+
+    Double[] DThirdTest = {3.6, 2.5, 5.3};
+    @Test
+    public void DMaxatThirdPosition()
+    {
+        TestMaximum DThirdMax = new TestMaximum();
+        Double result = DThirdMax.findMax(DThirdTest);
+        Assert.assertEquals(DThirdTest[2], result);
+    }
 }
